ee15f4153859636badda146527bee57cd4c15340
tompere96 <tompere96@gmail.com>
2018-11-26 13:23:41 +0100

    login template

--- Implementation/app.js
+++ Implementation/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+let loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -20,9 +21,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-
+app.use('/bootstrap',express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
